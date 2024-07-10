ffef15dc4f3710b3ab975fac75ac8201a2281861
Santa singh <111549113+santa-singh91535@users.noreply.github.com>
2024-07-10 09:04:50 +0530

    Add files via upload

---
+++ FolderCleaner.py
@@ -0,0 +1,27 @@
+import os
+import PyPDF2
+import os
+
+folder_path = 'path to resume folder'
+def folder_cleaner(folder_path):
+    for filename in os.listdir(folder_path):
+            if filename.endswith('.pdf'):
+                
+                pdf_path = os.path.join(folder_path, filename)
+            
+                try:
+                    with open(pdf_path, 'rb') as file:
+                        reader = PyPDF2.PdfReader(file)
+                        text=''
+                        for page_num in range(len(reader.pages)):
+                            page = reader.pages[page_num]
+                            text += page.extract_text()
+                            
+                        text=text.strip()
+                        if len(text)==0:
+                            os.remove(pdf_path)
+                except :
+                    os.remove(pdf_path)
+                    continue
+      
+    
